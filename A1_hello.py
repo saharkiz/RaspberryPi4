d24a164106f8962dc6d11a6fc3b1c6d737d5d548
Aresh Saharkhiz <saharkiz@yahoo.com>
2020-09-21 20:07:15 +0800

    Create A1_hello.py

---
+++ A1_hello.py
@@ -0,0 +1,14 @@
+#Connect Ground and Pin 7
+
+#import the GPIO and time package
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT)
+# loop through 10 times, on/off for 1 second
+for i in range(10):
+    GPIO.output(7,True)
+    time.sleep(1)
+    GPIO.output(7,False)
+    time.sleep(1)
+GPIO.cleanup()
