41a107ca17b88aa1fd1e25e3819b3915a532a4fc
Aresh Saharkhiz <saharkiz@yahoo.com>
2020-09-22 12:22:15 +0800

    Update A1_hello.py

--- A1_hello.py
+++ A1_hello.py
@@ -4,7 +4,7 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.OUT)
+GPIO.setup(7, GPIO.OUT) #GPIO4 on PIN7
 # loop through 10 times, on/off for 1 second
 for i in range(10):
     GPIO.output(7,True)
